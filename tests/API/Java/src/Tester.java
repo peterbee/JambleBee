48fe138593fa3c608603dca951cdc1aede926f34
Guy Grigsby <guy.grigsby@gmail.com>
2015-02-18 19:59:28 -0700

    created test case for upload API.

--- tests/API/Java/src/Tester.java
+++ tests/API/Java/src/Tester.java
@@ -1,28 +1,49 @@
 import static org.junit.Assert.fail;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.apache.http.HttpResponse;
 import org.junit.Test;
 
-
 public class Tester {
- 
+
+	private static final String HOST = "http://guygrigsby.com:3000";
+
 	@Test
-	public void test() {
-		String host = "http://guygrigsby.com:3000";
+	public void downloadTest() {
+
 		String saveLocation = "/tmp";
 		String videoName = "catVideo";
-		VideoDownload dl = new VideoDownload(host, saveLocation, videoName);
-		try {
-			HttpResponse response = dl.executeRequest();
+		downloadVideo(saveLocation, videoName);
+		// fail("Not yet implemented");
+	}
 
-			dl.saveFile(response);
+	@Test
+	public void uploadTest() {
+		String fileName = "/tmp/catVideo.mp4";
+		Path filePath = Paths.get(fileName);
+		VideoUpload ul = new VideoUpload(HOST, filePath);
+		try {
+			ul.executeRequest();
 		} catch (IOException e) {
 			fail(e.getLocalizedMessage());
 		}
-		
-		//fail("Not yet implemented");
+	}
+
+	private File downloadVideo(String saveLocation, String videoName) {
+
+		VideoDownload dl = new VideoDownload(HOST, videoName);
+		try {
+			HttpResponse response = dl.executeRequest();
+
+			dl.saveFile(response, saveLocation);
+		} catch (IOException e) {
+			fail(e.getLocalizedMessage());
+		}
+		return new File(saveLocation);
 	}
 
 }
