177599badc2148c31de309ea1b046c58cc22deeb
Guy Grigsby <guy.grigsby@gmail.com>
2015-03-10 20:01:02 -0600

    removed JSON exceptions from VideoProject, except for constructor.

--- tests/API/Java/src/contrivance/rest/model/VideoData.java
+++ tests/API/Java/src/contrivance/rest/model/VideoData.java
@@ -1,11 +1,12 @@
 package contrivance.rest.model;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class VideoData extends JSONObject {
 
-	public VideoData(String responseString) {
-		// TODO Auto-generated constructor stub
+	public VideoData(String responseString) throws JSONException {
+		super(responseString);
 	}
 
 	public String getId() {
