0ba92d6eb71cfc26fef7d08fb3b0659674a11a03
dgerrells <dgerrell@msudenver.edu>
2015-04-23 17:40:00 -0600

    Fixed ide issue with structure

---
+++ iOS Client/jambley/NMRangeSlider/DefaultTheme7/range-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  range-Bridging-Header.h
+//  jambley
+//
+//  Created by David Gerrells on 3/15/15.
+//  Copyright (c) 2015 David Gerrells. All rights reserved.
+//
+
+#import "NMRangeSlider.h"
+
+
+#ifndef jambley_range_Bridging_Header_h
+#define jambley_range_Bridging_Header_h
+
+
+
+#endif
